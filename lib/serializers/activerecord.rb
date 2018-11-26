5ddc99d3ff6fe305090f486552c0ece68ae0fd12
Reinaldo Oliveira <reinaldooli@gmail.com>
2018-11-26 12:23:35 -0200

    [fix] Fix missing reference
    Fix missing Serializer HashWithIndifferentAccess

--- lib/serializers/activerecord.rb
+++ lib/serializers/activerecord.rb
@@ -2,6 +2,7 @@ require "serializers/activerecord/br/cnpj"
 require "serializers/activerecord/br/cpf"
 
 require "serializers/activerecord/email"
+require "serializers/activerecord/hash_with_indifferent_access"
 
 module Serializers
   module Activerecord
